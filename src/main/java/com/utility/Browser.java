c7b48a6dca3b361130e72bb12d7d41726f5fa8d9
pradnya p <pradnyalomate101@gmail.com>
2023-04-08 14:50:12 +0530

    changes done by pradnya

--- src/main/java/com/utility/Browser.java
+++ src/main/java/com/utility/Browser.java
@@ -1,4 +1,55 @@
-package com.parabank.utility;
+package com.utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Browser {
+    private WebDriver driver;
+    private String browsername;
+
+    public WebDriver getDriver(){
+        return this.driver;
+    }
+
+    public Browser(String browsername){
+        this.browsername=browsername;
+    }
+
+    public void launch(){
+      String browserName = System.getProperty("browsername");
+      if(browserName!= null){
+          browserName=Constant.browsername;
+      }
+
+      if(browsername.equalsIgnoreCase("Chrome")){
+          System.setProperty("webdriver.driver.chrome",Constant.pathmainResources+"\\chrome_exe\\chromedriver.exe");
+          this.driver=new ChromeDriver();
+      }
+      else if(browserName.equalsIgnoreCase("firefox")){
+            System.setProperty("webdriver.driver.chrome",Constant.pathmainResources+"\\chrome_exe\\chromedriver.exe");
+            this.driver=new FirefoxDriver();
+        }else{
+          System.out.println("the provided browser name is not valid" + this.browsername + "taking chrome as default");
+          this.driver = null;
+      }
+    }
+
+    public void maximize(){
+
+        try{
+            this.driver.manage().window().maximize();
+        }catch(Exception e){
+            this.launch();
+            this.driver.manage().window().maximize();
+        }
+    }
+
+    public void nevigateUrl(String url){
+        this.driver.get("url");
+    }
+    public void close(){
+        this.driver.close();
+        this.driver.quit();
+    }
 }
