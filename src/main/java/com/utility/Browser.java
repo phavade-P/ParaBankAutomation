9f83c0d55c3ef60f348ec46e819571696d02ec79
pradnya p <pradnyalomate101@gmail.com>
2023-04-10 11:17:36 +0530

    Browser file

--- src/main/java/com/utility/Browser.java
+++ src/main/java/com/utility/Browser.java
@@ -1,55 +1,66 @@
 package com.utility;
 
+import com.constants.Constant;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class Browser {
     private WebDriver driver;
     private String browsername;
 
-    public WebDriver getDriver(){
+    public WebDriver getDriver() {
         return this.driver;
     }
 
-    public Browser(String browsername){
-        this.browsername=browsername;
+    public Browser(String browsername) {
+        this.browsername = browsername;
     }
 
-    public void launch(){
-      String browserName = System.getProperty("browsername");
-      if(browserName!= null){
-          browserName=Constant.browsername;
-      }
+    public void launch() {
+        this.browsername=Constant.browsername;
 
-      if(browsername.equalsIgnoreCase("Chrome")){
-          System.setProperty("webdriver.driver.chrome",Constant.pathmainResources+"\\chrome_exe\\chromedriver.exe");
-          this.driver=new ChromeDriver();
-      }
-      else if(browserName.equalsIgnoreCase("firefox")){
-            System.setProperty("webdriver.driver.chrome",Constant.pathmainResources+"\\chrome_exe\\chromedriver.exe");
-            this.driver=new FirefoxDriver();
-        }else{
-          System.out.println("the provided browser name is not valid" + this.browsername + "taking chrome as default");
-          this.driver = null;
-      }
+        if (browsername.equalsIgnoreCase("Chrome")) {
+            System.setProperty("webdriver.driver.chrome", Constant.pathmainResources + "\\chrome_exe\\chromedriver.exe");
+            this.driver = new ChromeDriver();
+        } else if (browsername.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.driver.chrome", Constant.pathmainResources + "\\chrome_exe\\chromedriver.exe");
+            this.driver = new FirefoxDriver();
+        } else {
+
+        }
     }
+       /* else if (this.browsername.equalsIgnoreCase("chromeGrid"))
+        {
 
-    public void maximize(){
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.setBrowserName("chrome");
+            try {
+                // this.driver= new RemoteWebDriver(new URL"");
 
-        try{
-            this.driver.manage().window().maximize();
-        }catch(Exception e){
-            this.launch();
-            this.driver.manage().window().maximize();
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+            driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+        }*/
+
+        public void maximize () {
+
+                this.driver.manage().window().maximize();
+
+        }
+
+        public void nevigateUrl (String url){
+            this.driver.get("url");
+        }
+        public void close () {
+            this.driver.close();
+            this.driver.quit();
         }
     }
 
-    public void nevigateUrl(String url){
-        this.driver.get("url");
-    }
-    public void close(){
-        this.driver.close();
-        this.driver.quit();
-    }
-}
