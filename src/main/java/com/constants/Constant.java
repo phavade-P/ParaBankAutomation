c3fb4e091079ade2a8470dc547a443691deee4df
pradnya p <pradnyalomate101@gmail.com>
2023-04-10 11:16:54 +0530

    Constant file

--- src/main/java/com/constants/Constant.java
+++ src/main/java/com/constants/Constant.java
@@ -1,7 +1,12 @@
-package com.utility;
+package com.constants;
 
 public class Constant {
-    public static final String browsername = "Chrome";
+    public static  String browsername = "";
+    public static String env = "";
     public static String pathmainResources = "src\\main\\resources\\";
     public static String pathTestResources = "src\\test\\resources\\";
+    public static int passedcount = 0;
+    public static int failedCount = 0;
+
+
 }
